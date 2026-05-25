6cde1031d16624a1e292b8892161248ce091032d
sakshirao8577 <raosakshi8577@gmail.com>
2026-05-25 09:51:19 +0530

    Add files via upload

---
+++ Mainapp.jsx
@@ -0,0 +1,20 @@
+import Task from "./Task"
+import Parent from "./Parent.jsx"
+import Productlist from "./Productlist.jsx"
+import Events1 from "./Events1.jsx"
+import Events2 from "./Events2.jsx"
+import My from "./routing/My.jsx"
+import Student from "./routing/Student.jsx"
+function Mainapp(){
+    return(
+        <>
+        {/* <Productlist/>
+        <br></br>
+        <Events1/>
+        <br/>
+        <Events2/> 
+        <My/> */}
+        <Student/>
+        </>
+    )
+}export default Mainapp;
